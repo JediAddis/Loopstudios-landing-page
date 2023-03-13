9f301a5d7319693570beabf46fedc57720e26e51
JediAddis <jodieaddiskp@hotmail.com>
2023-03-13 19:17:06 +0100

    Résolution du problème de l'emplacement du logo du site et de l'icone hamburger.

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,9 +1,12 @@
 // Création d'une constante pour l'ouverture//
 const open = document.querySelector(".open");
+
 //Constante pour le déroulement du menu de la NavBar//
 const MenuLinks = document.querySelector(".MenuLinks") ;
+
 //On ajoute un évènement à la const "open" : au click de l'icone hamburger, le menu s'affiche//
 open.addEventListener("click", () => {
     MenuLinks.classList.toggle("active");
 })
 
+const NavBar = document.querySelector(".NavBar");
